5fe39759e8b9c33cb79b4caab14310ace95e8ba0
N0usss <anas.baharin1@gmail.com>
2025-02-06 19:37:25 +0800

    Tidying the code for the NumberGuessingGame project

--- NumberGuessingGame/GuessNumber.java
+++ NumberGuessingGame/GuessNumber.java
@@ -8,20 +8,25 @@ public class GuessNumber {
 
         Scanner in = new Scanner(System.in);
 
+        //Using math.random method for the code to choose random number between 1-100
         int number = 1 + (int)(100*Math.random());
 
+        //Number of trials given for the user to guess
         int trial = 5;
 
         int i, guess;
 
-        System.out.println("A number has been chosen" + " between 1 to 100. " + "Guess the number" + " within 5 trials.");
+        System.out.println("A number has been chosen" + " between 1 to 100. ");
+        System.out.println("Guess the number" + " within 5 trials.");
 
+        //The guessing game code begins here by looping
         for(i=0;i<trial;i++){
 
             System.out.println("Guess the number:");
 
             guess = in.nextInt();
 
+            //If the user guess the correct number, the system will stop immediately.
             if(number == guess){
 
                 System.out.println("Congrats! You Win");
@@ -39,9 +44,10 @@ public class GuessNumber {
             }
         }
 
+        //When user out of trials for guesses, the system will stop
         if ( i == trial){
 
-            System.out.println("You're out trials");
+            System.out.println("You're out of trials");
 
             System.out.println("The number was " + number);
         }
